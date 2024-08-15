29eb282ee9a2e278a5efbff40bd529539e7d38ad
KAI <j20001020@gmail.com>
2024-08-16 04:25:58 +0800

    Create the project and setting DB configuration

---
+++ src/main/java/com/kai/ecommercebackendsystem/EcommerceBackendSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kai.ecommercebackendsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackendSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kai/ecommercebackendsystem/EcommerceBackendSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kai.ecommercebackendsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
